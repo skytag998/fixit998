2000cf8913d79ef5843f7b1fab8b9d3478894347
skytag998 <skytag9998@gmail.com>
2024-04-23 01:41:27 -0600

    Proyecto creado

---
+++ src/main/java/com/skytag998/fixitprueba998/Fixitprueba998Application.java
@@ -0,0 +1,13 @@
+package com.skytag998.fixitprueba998;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fixitprueba998Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fixitprueba998Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skytag998/fixitprueba998/Fixitprueba998ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skytag998.fixitprueba998;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fixitprueba998ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
